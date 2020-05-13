96db36b672217044c40fe7fc0108e8f1df1385fc
Dan Bruder <danb@structionsite.com>
2020-05-12 23:23:42 -0400

    add migration

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ async fn main() -> Result<()> {
 
     let pool = SqlitePool::new("sqlite://./db.sqlite3").await.unwrap();
 
+    sqlx::query!("CREATE TABLE IF NOT EXISTS todo (id TEXT PRIMARY KEY NOT NULL, body TEXT NOT NULL, complete BOOLEAN NOT NULL) ")
+            .execute(&pool)
+            .await?;
+
     web::start(pool).await;
     Ok(())
 }
